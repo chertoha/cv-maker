1b4e73e413c96ec57849c6b843d511ecbd43e115
Anton <a.chertok@akatech.com.ua>
2025-02-02 15:45:19 +0000

    init

---
+++ src/components/App/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      App component
+      <h1 className="text-3xl font-bold underline text-lime-500">Hello world!</h1>
+      <p className="text-7xl">alskdjflskdjf</p>
+    </div>
+  );
+};
+
+export default App;
